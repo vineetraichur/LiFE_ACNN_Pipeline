c39f3583724722fbec7825dbef7638b970a3a9cf
vineetraichur <vineetraichur@users.noreply.github.com>
2018-08-06 00:25:08 -0400

    Add files via upload

---
+++ Step0_InitConfig/Step0_main.m
@@ -0,0 +1,23 @@
+function Step0_main(pipe_config)
+
+cran_dir = '/ifs/loni/ccb/collabs/2017/ACNN/VineetRaichur_LiFE/LiFE_Pipeline_Cranium_BLSubj1'; % Project directory on Cranium
+cd([cran_dir '/Step0_InitConfig'])
+
+addpath(genpath('/ifs/loni/ccb/collabs/2017/ACNN/VineetRaichur_LiFE/LiFE_Libraries/jsonlab-master'))
+
+config = struct; % Initializing the matlab structure file that will contain locations of all files read/written in the pipeline
+
+config.t1 = [cran_dir '/BLSubj1_Data/Anatomy/t1.nii.gz'];
+config.coords = [0,0,0; 0,-16,0; 0,-8,40];
+config.dwi = [cran_dir '/BLSubj1_Data/Diffusion/dwi.nii.gz'];
+config.bvecs = [cran_dir '/BLSubj1_Data/Diffusion/dwi.bvecs'];
+config.bvals = [cran_dir '/BLSubj1_Data/Diffusion/dwi.bvals'];
+config.MNI_template = '/ifs/loni/ccb/collabs/2017/ACNN/VineetRaichur_LiFE/LiFE_Libraries/life-vistasoft-master/mrDiffusion/templates/MNI_T1.nii.gz';
+config.eddyCorrect = 1;
+config.rotateBvecsWithRx = 'false';
+config.rotateBvecsWithCanXform = 'false';
+config.resolution = 'default';
+
+savejson('', config, pipe_config); % saves teh matlab struct file as a json file
+
+end
---
+++ Step0_InitConfig/Step0_main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+pipe_config=$1
+
+cd /ifs/loni/ccb/collabs/2017/ACNN/VineetRaichur_LiFE/LiFE_Pipeline_Cranium_BLSubj1/Step0_InitConfig
+
+export MATLABPATH=/usr/local/MATLAB/R2015b/bin
+
+matlab -nodisplay -nosplash -nodesktop -r "Step0_main $pipe_config"
+
+exit

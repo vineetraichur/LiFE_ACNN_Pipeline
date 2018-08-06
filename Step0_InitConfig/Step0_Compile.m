2d7b32de67a5370f10a3876d189d43c176d61d73
vineetraichur <vineetraichur@users.noreply.github.com>
2018-08-06 00:23:36 -0400

    Delete Step0_Compile.m

--- Step0_InitConfig/Step0_Compile.m
+++
@@ -1,7 +0,0 @@
-clear
-
-cd('/ifs/loni/ccb/collabs/2017/ACNN/VineetRaichur_LiFE/LiFE_ACNN_Pipeline_Cranium/Step0_InitConfig')
-addpath(genpath('/ifs/loni/ccb/collabs/2017/ACNN/VineetRaichur_LiFE/LiFE_Libraries/jsonlab-master'))
-
-% Step0_main
-mcc -m -W main -T link:exe Step0_main.m
